80d574449ed9556afb11236137044e7ffdcbc5d4
jiwon5304 <80395324+jiwon5304@users.noreply.github.com>
2022-05-27 17:54:21 +0900

    create project

---
+++ src/main/java/com/example/java_hexagonal/JavaHexagonalApplication.java
@@ -0,0 +1,13 @@
+package com.example.java_hexagonal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaHexagonalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaHexagonalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/java_hexagonal/JavaHexagonalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java_hexagonal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaHexagonalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
