4b6c5f14322d5c50c88868e1198776cb401a98b6
jiwon5304 <80395324+jiwon5304@users.noreply.github.com>
2022-05-27 19:52:00 +0900

    apply mapStruct mapper

--- src/main/java/com/example/java_hexagonal/adapter/in/web/UserController.java
+++ src/main/java/com/example/java_hexagonal/adapter/in/web/UserController.java
@@ -6,6 +6,8 @@ import com.example.java_hexagonal.domain.User;
 import io.tej.SwaggerCodgen.api.UsersApi;
 import io.tej.SwaggerCodgen.model.UserRequest;
 import io.tej.SwaggerCodgen.model.UserResponse;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,15 +20,15 @@ public class UserController implements UsersApi {
         this.userUseCase = userUseCase;
     }
 
+    // toResponse 메소드 생성
+    private UserResponse toResponse(User user) {
+        return Mappers.getMapper(UserResponseMapper.class).toResponse(user);
+    }
+
     // 회원 생성
     @Override
     public ResponseEntity<UserResponse> createUser(UserRequest userRequest) {
         User user = userUseCase.createUser(userRequest);
-
-        UserResponse userResponse = new UserResponse()
-                .id(user.getId())
-                .name(user.getName())
-                .password(user.getPassword());
-        return ResponseEntity.ok(userResponse);
+        return ResponseEntity.ok(toResponse(user));
     }
 }
